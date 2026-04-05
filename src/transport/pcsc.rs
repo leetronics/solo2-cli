56caab569468f77a7cda4ff36878044800ee9856
Manuel Domke <manuel-domke@gmx.org>
2026-04-05 16:35:53 +0200

    Handle ISO 7816 response chaining (61 XX) in PCSC transport

--- src/transport/pcsc.rs
+++ src/transport/pcsc.rs
@@ -55,19 +55,51 @@ impl Device {
         let sw2 = recv_buffer.pop().unwrap();
         let sw1 = recv_buffer.pop().unwrap();
 
-        let status = (sw1, sw2).try_into();
-        if Ok(Status::Success) != status {
-            return Err(if !recv_buffer.is_empty() {
-                anyhow!(
-                    "card signaled error {:?} ({:X}, {:X}) with data {}",
-                    status,
-                    sw1,
-                    sw2,
-                    hex::encode(recv_buffer)
-                )
-            } else {
-                anyhow!("card signaled error: {:?} ({:X}, {:X})", status, sw1, sw2)
-            });
+        let status: std::result::Result<Status, _> = (sw1, sw2).try_into();
+        match status {
+            Ok(Status::Success) => {}
+            Ok(Status::MoreAvailable(remaining)) => {
+                // Accumulate the initial chunk, then issue GET RESPONSE until 9000
+                let mut response = recv_buffer;
+                let mut more = remaining;
+                loop {
+                    let get_response = [cla, 0xC0, 0x00, 0x00, more];
+                    debug!(">> {}", hex::encode(&get_response));
+                    let mut buf = vec![0; 3072];
+                    let n = self.device.transmit(&get_response, &mut buf)?.len();
+                    buf.resize(n, 0);
+                    debug!("<< {}", hex::encode(&buf));
+                    if n < 2 {
+                        return Err(anyhow!("short response during GET RESPONSE chaining"));
+                    }
+                    let sw2b = buf.pop().unwrap();
+                    let sw1b = buf.pop().unwrap();
+                    response.extend_from_slice(&buf);
+                    let next: std::result::Result<Status, _> = (sw1b, sw2b).try_into();
+                    match next {
+                        Ok(Status::Success) => break,
+                        Ok(Status::MoreAvailable(n)) => more = n,
+                        other => return Err(anyhow!(
+                            "GET RESPONSE error: {:?} ({:X}, {:X})",
+                            other, sw1b, sw2b
+                        )),
+                    }
+                }
+                return Ok(response);
+            }
+            other => {
+                return Err(if !recv_buffer.is_empty() {
+                    anyhow!(
+                        "card signaled error {:?} ({:X}, {:X}) with data {}",
+                        other,
+                        sw1,
+                        sw2,
+                        hex::encode(recv_buffer)
+                    )
+                } else {
+                    anyhow!("card signaled error: {:?} ({:X}, {:X})", other, sw1, sw2)
+                });
+            }
         }
 
         Ok(recv_buffer)
